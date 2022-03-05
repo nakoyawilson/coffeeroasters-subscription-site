aa33fb715b46af519040d34483eff6f60c5b42bd
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-03-05 14:43:18 -0400

    Add page routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Home from "./routes/home";
+import About from "./routes/about";
+import Plan from "./routes/plan";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="plan" element={<Plan />} />
+          <Route path="*" element={<p>There's nothing here!</p>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
